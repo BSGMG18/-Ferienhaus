c8b191f4e813738baf259881d22fcb5e3e37aceb
BSGMG18 <maximilianglomm@googlemail.com>
2019-08-07 22:19:40 +0200

    a

--- js/myjs.js
+++ js/myjs.js
@@ -65,24 +65,20 @@ $(document).ready(function(){
   });
 });
 
-function myfunction(x){
-  var prevScrollpos = window.pageYOffset;
-  if (x.matches) {
-    window.onscroll = function() {
-      var currentScrollPos = window.pageYOffset;
-      if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-      } else {
-        document.getElementById("navbar").style.top = "-60px";
-      }
-      prevScrollpos = currentScrollPos;
-    } 
-  }
-}
 
-var x = window.matchMedia("(min-width: 992px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementsByClassName("navbar-hide").style.top = "0";
+  } else {
+    document.getElementsByClassName("navbar-hide").style.top = "-60px";
+  }
+  prevScrollpos = currentScrollPos;
+} 
+
+
+
 
 /*   // Initialize and add the map
 function initMap() {
