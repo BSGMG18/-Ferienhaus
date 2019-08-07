f6dac36f18e63c23be98a65b7d367cad53f3c062
BSGMG18 <maximilianglomm@googlemail.com>
2019-08-07 22:33:47 +0200

    a

--- js/myjs.js
+++ js/myjs.js
@@ -70,9 +70,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementsById("navbar").style.top = "0";
+    document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementsById("navbar").style.top = "-60px";
+    document.getElementById("navbar").style.top = "-60px";
   }
   prevScrollpos = currentScrollPos;
 } 
